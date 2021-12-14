7483fcf7af09dbe51e7b8bb1c3dd873bfa5e47c6
Rajwinder Bhatoe <rajwinder.bhatoe@gmail.com>
2021-12-14 10:35:31 +0000

    format using prettier

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ const herokuSSLSetting = { rejectUnauthorized: false };
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting;
 const dbConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 };
 
 const app = express();
@@ -33,32 +33,39 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.put<{}, {}, {breed: string, subbreed: string|null}>("/score", async (req, res) => {
-  const { breed, subbreed } = req.body;
-  let text: string;
-  let values: string[];
-  if (subbreed !== null){
-    text = "UPDATE dog SET score = score + 1 WHERE breed_id = (SELECT id from breed WHERE name = ($1)) AND subbreed_name = ($2) RETURNING *;"
-    values = [breed, subbreed];
-  } else {
-    text = "UPDATE dog SET score = score + 1 WHERE breed_id = (SELECT id from breed WHERE name = ($1)) RETURNING *;"
-    values = [breed];
+app.put<{}, {}, { breed: string; subbreed: string | null }>(
+  "/score",
+  async (req, res) => {
+    const { breed, subbreed } = req.body;
+    let text: string;
+    let values: string[];
+    if (subbreed !== null) {
+      text =
+        "UPDATE dog SET score = score + 1 WHERE breed_id = (SELECT id from breed WHERE name = ($1)) AND subbreed_name = ($2) RETURNING *;";
+      values = [breed, subbreed];
+    } else {
+      text =
+        "UPDATE dog SET score = score + 1 WHERE breed_id = (SELECT id from breed WHERE name = ($1)) RETURNING *;";
+      values = [breed];
+    }
+    const increasedScore = await client.query(text, values);
+
+    res.status(200).json({
+      status: "success",
+      message: "score increased by 1",
+      response: increasedScore.rows[0],
+    });
   }
-  const increasedScore = await client.query(text, values);
-  
-  res.status(200).json({
-    status: "success",
-    message: "score increased by 1",
-    response: increasedScore.rows[0]
-  });
-});
+);
 
 app.get("/score", async (req, res) => {
-  const scores = await client.query("SELECT breed.name, dog.subbreed_name, dog.score FROM dog LEFT JOIN breed ON dog.breed_id = breed.id ORDER BY score desc LIMIT 10");
+  const scores = await client.query(
+    "SELECT breed.name, dog.subbreed_name, dog.score FROM dog LEFT JOIN breed ON dog.breed_id = breed.id ORDER BY score desc LIMIT 10"
+  );
   res.status(200).json({
     status: "success",
     message: "get top 10 scores desc",
-    scores: scores.rows
+    scores: scores.rows,
   });
 });
 
